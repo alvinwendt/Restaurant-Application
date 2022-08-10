7588060ef93ed1a8e1666a8a50146b852aa05b7c
unknown <kylebennett9514@gmail.com>
2022-08-09 19:18:34 -0500

    updated my js page

--- scripts/KB_Section.js
+++ scripts/KB_Section.js
@@ -1,178 +1,78 @@
-const diners = [
+const KB_Data = [
     {
-        table: 1,
-        seated: true,
-        seat: 1,
-        order: "Nachos",
-        amount: 9.99
+      table: 17,
+      seated: true,
+      seats: 4,
+      order: [
+        "Hickory Bacon Cheeseburger",
+        "Pulled Pork Sandwich",
+        "Mexican Style Nachos",
+        "House Fajitas",
+      ],
+      amount: 46.88,
     },
     {
-        table: 1,
-        seated: true,
-        seat: 2,
-        order: "Burger",
-        amount: 9.99
+      table: 18,
+      seated: true,
+      seats: 3,
+      order: ["Lasagna", "Chicken burger", "Chicken parmesan", "Burger Sliders"],
+      amount: 65.89,
     },
     {
-        table: 1,
-        seated: true,
-        seat: 3,
-        order: "Steak",
-        amount: 9.99
+      table: 19,
+      seated: false,
+      seats: 4,
+      order: ["Chicken Pesto", "Lobster rolls", "Mexican Pizza"],
+      amount: 47.52,
     },
     {
-        table: 2,
-        seated: true,
-        seat: 1,
-        order: "Nachos",
-        amount: 9.99
+      table: 20,
+      seated: true,
+      seats: 2,
+      order: [
+        "Nachos",
+        "Tomato soup with Grilled cheese",
+        "Flatbread pizza",
+        "Mac & cheese",
+      ],
+      amount: 55.69,
     },
-    {
-        table: 2,
-        seated: true,
-        seat: 2,
-        order: "Burger",
-        amount: 9.99
-    },
-    {
-        table: 2,
-        seated: true,
-        seat: 3,
-        order: "Steak",
-        amount: 9.99
-    },
-    {
-        table: 3,
-        seated: true,
-        seat: 1,
-        order: "Nachos",
-        amount: 9.99
-    },
-    {
-        table: 3,
-        seated: true,
-        seat: 2,
-        order: "Burger",
-        amount: 9.99
-    },
-    {
-        table: 3,
-        seated: true,
-        seat: 3,
-        order: "Steak",
-        amount: 9.99
-    },
-    {
-        table: 4,
-        seated: false,
-        seat: 1,
-        order: "Nachos",
-        amount: 9.99
-    },
-    {
-        table: 4,
-        seated: true,
-        seat: 2,
-        order: "Burger",
-        amount: 9.99
-    },
-    {
-        table: 4,
-        seated: false,
-        seat: 3,
-        order: "Steak",
-        amount: 9.99
-    }]
-
-const openSeating = 'Available for seating.'
-
-let currentOrders = []
-let currentOrders1 = []
-let currentOrders2 = []
-let currentOrders3 = []
-
-for (const taco of diners) {
-    if (taco.seated == true && taco.table === 1) {
-        //document.getElementsByClassName('one')[0].innerHTML = diners[0].table
-        currentOrders.push(`seat:${taco.seat} order:${taco.order} amount:${taco.amount}`)
+  ];
+  
+  //Logic for Tables Population
+  
+  let KB_htmlWaiter = '<h2 class="waiterTitle">Kyles Tables</h2><ul>';
+  
+  for (const KB_waiter of KB_Data) {
+  
+    if (waiter.seated === true) {
+      KB_htmlWaiter += `<li><b>Table ${KB_waiter.table}</b></li><ul>`;
+      KB_htmlWaiter += `<li># Seated: ${KB_waiter.seats}</li>`;
+      KB_htmlWaiter += `<li>Food: ${KB_waiter.order}</li>`;
+      KB_htmlWaiter += `<li>Bill: $${KB_waiter.amount}</li></ul>`;
+    } else if (KB_waiter.seated === false) {
+      KB_htmlWaiter += `<li><b>Table $KB_{waiter.table}</b></li><ul>`;
+      KB_htmlWaiter += `<li>Open for Seating</li></ul>`;
     }
-    //else if (currentOrders.length === 0)
-    //{document.getElementsByClassName('one')[0].innerHTML = openSeating
-    //}
-}
-for (const taco of diners) {
-    if (taco.seated == true && taco.table === 2) {
-        //document.getElementsByClassName('one')[0].innerHTML = diners[0].table
-        currentOrders1.push(`seat:${taco.seat} order:${taco.order} amount:${taco.amount}`)
+  }
+  
+  // Calculation for Tips
+  let KB_totalBill = 0;
+  
+  for (const KB_bill of KB_Data) {
+    if (KB_bill.seated == true) {
+      KB_totalBill += KB_bill.amount;
     }
-    //else if (currentOrders.length === 0)
-    //{document.getElementsByClassName('one')[0].innerHTML = openSeating
-    //}
-}
-for (const taco of diners) {
-    if (taco.seated == true && taco.table === 3) {
-        //document.getElementsByClassName('one')[0].innerHTML = diners[0].table
-        currentOrders2.push(`seat:${taco.seat} order:${taco.order} amount:${taco.amount}`)
-    }
-    //else if (currentOrders.length === 0)
-    //{document.getElementsByClassName('one')[0].innerHTML = openSeating
-    //}
-}
-for (const taco of diners) {
-    if (taco.seated == true && taco.table === 4) {
-        //document.getElementsByClassName('one')[0].innerHTML = diners[0].table
-        currentOrders3.push(`seat:${taco.seat} order:${taco.order} amount:${taco.amount}`)
-    }
-    //else if (currentOrders.length === 0)
-    //{document.getElementsByClassName('one')[0].innerHTML = openSeating
-    //}
-}
-
-//if (isNaN(currentOrders3)) { 
-    //document.getElementsByClassName('receipt3')[0].innerHTML = openSeating
- //}
-
-document.getElementsByClassName('receipt')[0].innerHTML = currentOrders[0] + currentOrders[1] + currentOrders[2]
-document.getElementsByClassName('receipt1')[0].innerHTML = currentOrders1[0] + currentOrders1[1] + currentOrders1[2]
-document.getElementsByClassName('receipt2')[0].innerHTML = currentOrders2[0] + currentOrders2[1] + currentOrders2[2]
-document.getElementsByClassName('receipt3')[0].innerHTML = currentOrders3[0] + currentOrders3[1] + currentOrders3[2]
-
-
-
-//     ,
-//     {
-//         table: 2,
-//         seated: true,
-//                 food: [{
-//             seat: 1,
-//             order: "Lobster",
-//             amount: 9.99
-//         },
-//         {
-//             seat: 2,
-//             order: "Salmon",
-//             amount: 9.99
-//         },
-//         {
-//             seat: 3,
-//             order: "Chicken Tenders",
-//             amount: 9.99
-//         }]
-//     },
-//     {
-//         table: 3,
-//         seated: false,
-//     },
-//     {
-//         table: 4,
-//         seated: false,
-//     }
-// ]
-// document.getElementsByClassName('one')[0].innerHTML = diners[0].table
-// document.getElementsByClassName('ones').innerHTML = `seat ${diners[0].food[0].seat}`
-// //document.getElementsByClassName('two')[1].innerHTML = diners[1].table
-// //document.getElementsByClassName('twos').innerHTML = diners[1].food
-// //document.getElementsByClassName('three').innerHTML = diners[2].table
-// //document.getElementsByClassName('threes').innerHTML = diners[2].food
-// //document.getElementsByClassName('four').innerHTML = diners[3].table
-// //document.getElementsByClassName('fours').innerHTML = diners[3].food
+  }
+  KB_totalBill = KB_totalBill.toFixed(2);
+  
+  KB_tipAmount = KB_totalBill * 0.2;
+  KB_tipAmount = KB_tipAmount.toFixed(2);
+  
+  KB_grandTotal = Number(KB_totalBill) + Number(KB_tipAmount);
+  KB_grandTotal = KB_grandTotal.toFixed(2);
+  
+  document.getElementsByClassName("receipt4")[0].innerKB_htmlWaiter = KB_htmlWaiter;
+  document.getElementsByClassName(
+    "tip4"
+  )[0].innerKB_htmlWaiter = `<p id="waiter"><b>Kyle's Final Take</b></p> <p>Subtotal: $${KB_totalBill} <p>Total: $${KB_grandTotal}  <h3 id="waiter">Kyle's Tips: $${KB_tipAmount}`;
