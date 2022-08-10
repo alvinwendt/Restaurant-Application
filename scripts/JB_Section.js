055847a1fd8ce9591a13dbcd0aa7b62d1570f5a8
DeminZawity <106280855+DeminZawity@users.noreply.github.com>
2022-08-09 21:35:59 -0500

    final touches woohoo git status cd ..

--- scripts/JB_Section.js
+++ scripts/JB_Section.js
@@ -1,50 +1,47 @@
 const JB_Data = [
-    {
-      table: 13,
-      seated: true,
-      seats: 4,
-      order: ["Cavatappi Bolognese", "Chicken Parmesan", "Caprese Chicken", "Lasagna"],
-      amount: 89.74,
-    },
-    {
-      table: 14,
-      seated: true,
-      seats: 4,
-      order: ["Pad Kra Pao Moo", "Chicken Fried Rice", "Pad Thai", "Kai Yad Sai"],
-      amount: 78.95,
-    },
-    {
-      table: 15,
-      seated: false,
-      seats: 4,
-      order: ["Orange Chicken", "Beijing Roasted Duck", "Kung Pao Chicken", "Sweet and Sour Shrimp"],
-      amount: 115.38,
-    },
-    {
-      table: 16,
-      seated: true,
-      seats: 4,
-      order: ["Chocolate Molten Lava Cake", "Strawberry Shortcake", "Mint Chocolate Chip Icecream", "New York Cheesecake"],
-      amount: 60.32,
-    },
-  ];
-  
+  {
+    table: 13,
+    seated: true,
+    seats: 4,
+    order: ["Cavatappi Bolognese", "Chicken Parmesan", "Caprese Chicken", "Lasagna"],
+    amount: 89.74,
+  },
+  {
+    table: 14,
+    seated: true,
+    seats: 4,
+    order: ["Pad Kra Pao Moo", "Chicken Fried Rice", "Pad Thai", "Kai Yad Sai"],
+    amount: 78.95,
+  },
+  {
+    table: 15,
+    seated: false,
+    seats: 4,
+    order: ["Orange Chicken", "Beijing Roasted Duck", "Kung Pao Chicken", "Sweet and Sour Shrimp"],
+    amount: 115.38,
+  },
+  {
+    table: 16,
+    seated: true,
+    seats: 4,
+    order: ["Chocolate Molten Lava Cake", "Strawberry Shortcake", "Mint Chocolate Chip Icecream", "New York Cheesecake"],
+    amount: 60.32,
+  },
+];
 
-  
-
-  let JB_htmlWaiter = '<h2 class="waiterTitle">John Tables</h2><ul>';
+let JB_htmlWaiter = '<h2 class="waiterTitle">John Tables</h2><ul>';
 
 for (const waiter of JB_Data) {
-//   console.log(waiter);
+  //   console.log(waiter);
 
   if (waiter.seated === true) {
-    JB_htmlWaiter += `<li><b>Table ${waiter.table}</b></li><ul>`;
+    JB_htmlWaiter += `<li class = "decorate"><b>Table ${waiter.table}</b></li><ul>`;
     JB_htmlWaiter += `<li># Seated: ${waiter.seats}</li>`;
     JB_htmlWaiter += `<li>Food: ${waiter.order}</li>`;
     JB_htmlWaiter += `<li>Bill: $${waiter.amount}</li></ul>`;
   } else if (waiter.seated === false) {
-    JB_htmlWaiter += `<li><b>Table ${waiter.table}</b></li><ul>`;
-    JB_htmlWaiter += `<li>Open for Seating</li></ul>`;
+    JB_htmlWaiter += `<li class = "decorate"><b>Table ${waiter.table}</b></li><ul>`;
+    JB_htmlWaiter += `<li class= "green">Open for Seating</li></ul>`;
   }
 }
 
--- scripts/KB_Section.js
+++ scripts/KB_Section.js
@@ -1,81 +1,67 @@
 const KB_Data = [
-    {
-      table: 17,
-      seated: true,
-      seats: 4,
-      order: [
-        "Hickory Bacon Cheeseburger",
-        "Pulled Pork Sandwich",
-        "Mexican Style Nachos",
-        "House Fajitas",
-      ],
-      amount: 46.88,
-    },
-    {
-      table: 18,
-      seated: true,
-      seats: 3,
-      order: [
-        "Caesar Salad",
-        "House Salad",
-        "Bacon Ranch Quesadilla",],
-      amount: 28.74,
-    },
-    {
-      table: 19,
-      seated: false,
-      seats: 4,
-      order: ["Loaded Baked Potato",
-      "Barbecue Sliders",
-      "Chicken Tenders"],
-      amount: 26.93,
-    },
-    {
-      table: 20,
-      seated: true,
-      seats: 2,
-      order: [
-        "Grilled Chicken Sandwich",
-        "Turkey Wrap",
-      ],
-      amount: 19.67,
-    },
-  ];
-  
-  //Logic for Tables Population
-  
-  let KB_htmlWaiter = '<h2 class="waiterTitle">Kyles Tables</h2><ul>';
-  
-  for (const KB_waiter of KB_Data) {
-  
-    if (KB_waiter.seated === true) {
-      KB_htmlWaiter += `<li><b>Table ${KB_waiter.table}</b></li><ul>`;
-      KB_htmlWaiter += `<li># Seated: ${KB_waiter.seats}</li>`;
-      KB_htmlWaiter += `<li>Food: ${KB_waiter.order}</li>`;
-      KB_htmlWaiter += `<li>Bill: $${KB_waiter.amount}</li></ul>`;
-    } else if (KB_waiter.seated === false) {
-      KB_htmlWaiter += `<li><b>Table ${KB_waiter.table}</b></li><ul>`;
-      KB_htmlWaiter += `<li>Open for Seating</li></ul>`;
-    }
+  {
+    table: 17,
+    seated: true,
+    seats: 4,
+    order: ["Hickory Bacon Cheeseburger", "Pulled Pork Sandwich", "Mexican Style Nachos", "House Fajitas"],
+    amount: 46.88,
+  },
+  {
+    table: 18,
+    seated: true,
+    seats: 3,
+    order: ["Caesar Salad", "House Salad", "Bacon Ranch Quesadilla"],
+    amount: 28.74,
+  },
+  {
+    table: 19,
+    seated: false,
+    seats: 4,
+    order: ["Loaded Baked Potato", "Barbecue Sliders", "Chicken Tenders"],
+    amount: 26.93,
+  },
+  {
+    table: 20,
+    seated: true,
+    seats: 2,
+    order: ["Grilled Chicken Sandwich", "Turkey Wrap"],
+    amount: 19.67,
+  },
+];
+
+//Logic for Tables Population
+
+let KB_htmlWaiter = '<h2 class="waiterTitle">Kyles Tables</h2><ul>';
+
+for (const KB_waiter of KB_Data) {
+  if (KB_waiter.seated === true) {
+    KB_htmlWaiter += `<li class = "decorate"><b>Table ${KB_waiter.table}</b></li><ul>`;
+    KB_htmlWaiter += `<li># Seated: ${KB_waiter.seats}</li>`;
+    KB_htmlWaiter += `<li>Food: ${KB_waiter.order}</li>`;
+    KB_htmlWaiter += `<li>Bill: $${KB_waiter.amount}</li></ul>`;
+  } else if (KB_waiter.seated === false) {
+    KB_htmlWaiter += `<li class = "decorate"><b>Table ${KB_waiter.table}</b></li><ul>`;
+    KB_htmlWaiter += `<li class= "green">Open for Seating</li></ul>`;
   }
-  
-  // Calculation for Tips
-  let KB_totalBill = 0;
-  
-  for (const KB_bill of KB_Data) {
-    if (KB_bill.seated == true) {
-      KB_totalBill += KB_bill.amount;
-    }
+}
+
+// Calculation for Tips
+let KB_totalBill = 0;
+
+for (const KB_bill of KB_Data) {
+  if (KB_bill.seated == true) {
+    KB_totalBill += KB_bill.amount;
   }
-  KB_totalBill = KB_totalBill.toFixed(2);
-  
-  KB_tipAmount = KB_totalBill * 0.2;
-  KB_tipAmount = KB_tipAmount.toFixed(2);
-  
-  KB_grandTotal = Number(KB_totalBill) + Number(KB_tipAmount);
-  KB_grandTotal = KB_grandTotal.toFixed(2);
-  
-  document.getElementsByClassName("receipt4")[0].innerHTML = KB_htmlWaiter;
-  document.getElementsByClassName(
-    "tip4"
-  )[0].innerHTML = `<p id="waiter"><b>Kyle's Final Take</b></p> <p>Subtotal: $${KB_totalBill} <p>Total: $${KB_grandTotal}  <h3 id="waiter">Kyle's Tips: $${KB_tipAmount}`;
+}
+KB_totalBill = KB_totalBill.toFixed(2);
+
+KB_tipAmount = KB_totalBill * 0.2;
+KB_tipAmount = KB_tipAmount.toFixed(2);
+
+KB_grandTotal = Number(KB_totalBill) + Number(KB_tipAmount);
+KB_grandTotal = KB_grandTotal.toFixed(2);
+
+document.getElementsByClassName("receipt4")[0].innerHTML = KB_htmlWaiter;
+document.getElementsByClassName(
+  "tip4"
+)[0].innerHTML = `<p id="waiter"><b>Kyle's Final Take</b></p> <p>Subtotal: $${KB_totalBill} <p>Total: $${KB_grandTotal}  <h3 id="waiter">Kyle's Tips: $${KB_tipAmount}`;
