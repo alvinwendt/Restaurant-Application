d85e27083b13d79aa93a1f3c857714808503d0e8
DeminZawity <106280855+DeminZawity@users.noreply.github.com>
2022-08-20 11:38:19 -0500

    adding data structure file

---
+++ scripts/main.js
@@ -0,0 +1,142 @@
+const servers = [
+  {
+    serverId: 1,
+    name: "Alvin",
+    tableId: [1, 2, 3, 4],
+  },
+  {
+    serverId: 2,
+    name: "Demin",
+    tableId: [5, 6, 7, 8],
+  },
+  {
+    serverId: 3,
+    name: "Hannah",
+    tableId: [9, 10, 11, 12],
+  },
+  {
+    serverId: 4,
+    name: "John",
+    tableId: [13, 14, 15, 16],
+  },
+  {
+    serverId: 5,
+    name: "Kyle",
+    tableId: [17, 18, 19, 20],
+  },
+];
+const tables = [
+  {
+    tableId: 1,
+    serverId: 1,
+    seats: 4,
+  },
+  {
+    tableId: 2,
+    serverId: 1,
+    seats: 4,
+  },
+  {
+    tableId: 3,
+    serverId: 1,
+    seats: 3,
+  },
+  {
+    tableId: 4,
+    serverId: 1,
+    seats: 4,
+  },
+  {
+    tableId: 5,
+    serverId: 2,
+    seats: 4,
+  },
+  {
+    tableId: 6,
+    serverId: 2,
+    seats: 4,
+  },
+  {
+    tableId: 7,
+    serverId: 2,
+    seats: 4,
+  },
+  {
+    tableId: 8,
+    serverId: 2,
+    seats: 4,
+  },
+  {
+    tableId: 9,
+    serverId: 3,
+    seats: 1,
+  },
+  {
+    tableId: 10,
+    serverId: 3,
+    seats: 2,
+  },
+  {
+    tableId: 11,
+    serverId: 3,
+    seats: 3,
+  },
+  {
+    tableId: 12,
+    serverId: 3,
+    seats: 3,
+  },
+  {
+    tableId: 13,
+    serverId: 4,
+    seats: 4,
+  },
+  {
+    tableId: 14,
+    serverId: 4,
+    seats: 4,
+  },
+  {
+    tableId: 15,
+    serverId: 4,
+    seats: 4,
+  },
+  {
+    tableId: 16,
+    serverId: 4,
+    seats: 4,
+  },
+  {
+    tableId: 17,
+    serverId: 5,
+    seats: 4,
+  },
+  {
+    tableId: 18,
+    serverId: 5,
+    seats: 3,
+  },
+  {
+    tableId: 19,
+    serverId: 5,
+    seats: 4,
+  },
+  {
+    tableId: 20,
+    serverId: 5,
+    seats: 2,
+  },
+];
+const menu = [
+    {
+        itemId: 1,
+        itemName: "Bourbon Street Chicken and Shrimp",
+        cost: 12.99,
+    },
+    {
+        itemId:,
+        itemName:,
+        cost:,
+    }
+];
+const orders = [];
